a6bec8c26db294d08d80b197174c5ade9a233661
RamiRond <raami.roond@gmail.com>
2021-03-08 20:51:15 +0100

    using infura (for now)

--- requester.py
+++ requester.py
@@ -1,8 +1,13 @@
 import requests
 import json
 import time
+from os import walk
 
-url = "http://192.168.178.38:9091/jsonrpc"
+#url = "http://192.168.178.38:9091/jsonrpc"
+url = "https://eth2-beacon-pyrmont.infura.io/eth/v1/validator/duties/proposer/"
+url2 = "https://eth2-beacon-pyrmont.infura.io/eth/v1/beacon/headers"
+user = "1pQO0ZiEx0FExWddgBK31GPQykI"
+pas = "3f2f041c2c959d8c3551890df3a6c670"
 headers = {'content-type': 'application/json'}
 
 
@@ -36,6 +41,15 @@ def getCurEpoch():
 
 
 if __name__ == "__main__":
-    while (True):
-        getProposals(getCurEpoch())
-        time.sleep(12)
+    # TODO reload when new validators are added
+    _, filenames, _ = next(walk("/home/leret/tmp/vals/validators"))
+
+    while True:
+        cur_slot = int(requests.get(url2, auth=(user, pas)).json()["data"][0]["header"]["message"]["slot"])
+        next_epoch = int(cur_slot / 32) + 1
+        print("slot: " + str(cur_slot) + ", epoch: " + str(next_epoch - 1))
+        arr = requests.get(url + str(next_epoch), auth=(user, pas)).json()["data"]
+        for duty in arr:
+            if duty["pubkey"] in filenames:
+                print("BLOCK PROPOSAL: " + duty["pubkey"] + " on slot: " + duty["slot"])
+        time.sleep(32 * 12 - 100)
